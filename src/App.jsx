72d8a2247c0da87a4f75ff12cd4e69c6a419df80
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-06 18:39:41 +0530

    Button removed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from "./components/navbar/navbar.jsx";
+import Dashboard from "./components/dashboard/dashboard.jsx";
 
 function App() {
   return (
     <>
-      <Navbar/>
+        <Navbar/>
+        <Dashboard/>
     </>
   )
 }
