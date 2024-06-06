68ddbd1bb726517844cc245da94eaba4c7b4df4e
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-06 19:43:04 +0530

    Styles added.

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import "/src/styles/dashboard/dashboard.css"
+
 import {Button} from "@mui/material";
+import {Add} from "@mui/icons-material";
 
 function Dashboard(props) {
     return (
-        <>
-            <Button className="dsb-btn" variant="contained">Contained</Button>
-        </>
+        <div className="dsb">
+            <Button className="dsb-btn" variant="contained" startIcon={<Add/>}>Transaction</Button>
+        </div>
     );
 }
 
