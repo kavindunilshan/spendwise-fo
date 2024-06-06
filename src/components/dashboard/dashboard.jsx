b933256e5b8b9f4ff7db6c4e9777c1fb462318ac
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-06 23:16:56 +0530

    Dialog component added.

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -3,15 +3,29 @@ import "/src/styles/dashboard/dashboard.css"
 
 import {Button} from "@mui/material";
 import {Add} from "@mui/icons-material";
+import CustomizedDialogs from "../forms/add-transaction.jsx";
 
 function Dashboard(props) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className="dsb">
-            <Button variant="contained"
-                    style={{
-                        backgroundColor: "#FB8B24"
-                    }}
-                    startIcon={<Add/>}>Transaction</Button>
+            <Button
+                onClick={handleClickOpen}
+                variant="contained"
+                style={{
+                    backgroundColor: "#FB8B24"
+                }}
+                startIcon={<Add/>}>Transaction</Button>
+            <CustomizedDialogs open={open} handleClose={handleClose}/>
+
         </div>
     );
 }
