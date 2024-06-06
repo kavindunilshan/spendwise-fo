7fb04d7d6e2094c3b1d21083eae67f7a44efc519
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-06 18:39:54 +0530

    Material ui used.

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Button} from "@mui/material";
 
 function Dashboard(props) {
     return (
-        <div></div>
+        <>
+            <Button className="dsb-btn" variant="contained">Contained</Button>
+        </>
     );
 }
 
