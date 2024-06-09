57eb0341d756a704b124b96e8f7f39ed0dfb02e1
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-09 14:24:47 +0530

    Header styles changed.

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from "../navbar/navbar.jsx";
+import "/src/styles/home/header.css";
 
 function Header() {
     return (
         <header className="home-header">
             <Navbar/>
-            <img src={'./src/assets/a.png'} alt={'home-base-image'} className={'home-logo'}/>
+            <img src={'./src/assets/a.webp'} alt={'home-base-image'} className={'home-logo'}/>
             <div className={'home-slogan'}>Every Dollar Tells a Story</div>
             <div className="home-action-btn">
             </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from "../components/home/header.jsx";
-import "/src/styles/home.css";
 
 function Home() {
     return (
