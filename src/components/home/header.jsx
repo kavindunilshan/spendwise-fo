fa35fd08ba509f5306c8602583ef6d07675156db
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-09 15:02:49 +0530

    Header styles done

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -6,9 +6,19 @@ function Header() {
     return (
         <header className="home-header">
             <Navbar/>
-            <img src={'./src/assets/a.webp'} alt={'home-base-image'} className={'home-logo'}/>
-            <div className={'home-slogan'}>Every Dollar Tells a Story</div>
-            <div className="home-action-btn">
+            <div className="home-header-content">
+
+                <div className={'home-header-content-left'}>
+                    <img src={'./src/assets/logoa.png'} alt={'home-base-image'} className={'home-logo'}/>
+                    <div className={'home-slogan'}>Personal Expense Tracker</div>
+                    <div className={'home-header-side-text'}>It's not about how much money you make,
+                        but how much money you keep, how hard it works for you,
+                        and how many generations you keep it for. <br/>
+                        <span className="small-text">-- Robert Kiyosaki --</span>
+                    </div>
+                </div>
+                <img src={'./src/assets/a.webp'} alt={'home-base-image'} className={'home-base'}/>
+                <div className="home-action-btn"></div>
             </div>
         </header>
     );
