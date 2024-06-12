5850c052087de67556c3a81cda1260a6c04d8d89
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-12 13:15:46 +0530

    Left image and its styles.

--- src/components/home/left-image.jsx
+++ src/components/home/left-image.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import "/src/styles/home/left-image.css";
 
 function LeftImage({ image, title, description }) {
     return (
-        <div className={'home-left-image'}>
-            <div className={'home-left-image-text'}>
-                <h2>{title}</h2>
-                <p>{description}</p>
+        <div className={'home-li-container'}>
+            <h2 className={'home-li-title'}>{title}</h2>
+            <div className={'home-li-content'}>
+                <img src={image} alt={'home-li'} className={'home-li'}/>
+                <div className={'home-li-description'}>{description}</div>
             </div>
         </div>
     );
