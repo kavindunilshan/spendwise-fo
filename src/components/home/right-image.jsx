392d8929f083aeabb28878d560230c00f24f0526
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-12 17:22:25 +0530

    Right image done

--- src/components/home/right-image.jsx
+++ src/components/home/right-image.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import "/src/styles/home/left-image.css";
 
 function RightImage({ image, title, description }) {
     return (
-        <div className={'home-right-image'}>
-            <div className={'home-right-image-text'}>
-                <h2>{title}</h2>
-                <p>{description}</p>
+        <div className={'home-li-container'}>
+            <div className={'home-li-content'}>
+                <img src={image} alt={'home-li'} className={'home-li'}/>
+                <div className={'home-li-text-container'}>
+                    <h2 className={'home-li-title'}>{title}</h2>
+                    <div className={'home-li-description'}>{description}</div>
+                </div>
             </div>
         </div>
     );
