fab4523876fc557e33d892b55185ba057841dee1
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-12 19:04:43 +0530

    Left image created more reusable way.

--- src/components/home/body.jsx
+++ src/components/home/body.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LeftImage from "./left-image.jsx";
-import RightImage from "./right-image.jsx";
 
 function Body(props) {
     const description1 = 'At Spendwise, we are a ' +
@@ -11,10 +10,18 @@ function Body(props) {
 
     return (
         <div >
-            <LeftImage title={'Who We Are'} image={'./src/assets/li1.png'}
+            <LeftImage isLeft={true} title={'Who We Are'} image={'./src/assets/li1.png'}
             description={description1}/>
-            <RightImage title={'Who We Are'} image={'./src/assets/li1.png'}
+            <LeftImage isLeft={false} title={'Who We Are 2'} image={'./src/assets/li1.png'}
                        description={description1}/>
+            <LeftImage isLeft={true} title={'Who We Are'} image={'./src/assets/li1.png'}
+                       description={description1}/>
+            <LeftImage isLeft={false} title={'Who We Are'} image={'./src/assets/li1.png'}
+                        description={description1}/>
+            <LeftImage isLeft={true} title={'Who We Are'} image={'./src/assets/li1.png'}
+                       description={description1}/>
+            <LeftImage isLeft={false} title={'Who We Are'} image={'./src/assets/li1.png'}
+                        description={description1}/>
         </div>
     );
 }
--- src/components/home/left-image.jsx
+++ src/components/home/left-image.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import "/src/styles/home/left-image.css";
+import {motion, useAnimation, useInView} from "framer-motion";
 
-function LeftImage({ image, title, description }) {
+function LeftImage({ isLeft, image, title, description }) {
+    const ref = useRef();
+
+    const isInView = useInView(ref, {once: false});
+
+    const tags = (
+        <>
+            {!isLeft ? (
+                <>
+                    <div className={'home-li-text-container'}>
+                        <h2 className={'home-li-title'}>{title}</h2>
+                        <div className={'home-li-description'}>{description}</div>
+                    </div>
+                    <img src={image} alt={'home-li'} className={'home-li'} />
+                </>
+            ) : (
+                <>
+                    <img src={image} alt={'home-li'} className={'home-li'} />
+                    <div className={'home-li-text-container'}>
+                        <h2 className={'home-li-title'}>{title}</h2>
+                        <div className={'home-li-description'}>{description}</div>
+                    </div>
+                </>
+            )}
+        </>
+    );
+
+
+    const anime = useAnimation();
+
+    useEffect(() => {
+        if (isInView) {
+            console.log('in view');
+            anime.start('visible');
+        } else {
+            console.log('out of view');
+            anime.start('hidden');
+        }
+    }, [isInView]);
+
     return (
-        <div className={'home-li-container'}>
-            <div className={'home-li-content'}>
-                <div className={'home-li-text-container'}>
-                    <h2 className={'home-li-title'}>{title}</h2>
-                    <div className={'home-li-description'}>{description}</div>
-                </div>
-                <img src={image} alt={'home-li'} className={'home-li'}/>
-            </div>
+        <div ref={ref} className={'home-li-container'}>
+            <motion.div
+                variants={{
+                    hidden: {opacity: 0, y: 75},
+                    visible: {opacity: 1, y: 0}
+                }}
+                initial={'hidden'}
+                animate={anime}
+                transition={{duration: 0.5, delay: 0.25}}
+                className={'home-li-content'}>
+                {tags}
+            </motion.div>
         </div>
     );
 }
