c8d73f8e11f06077af95eab7ef21afd9ef770168
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-13 16:39:13 +0530

    Okta connected

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <Auth0Provider
+        domain="dev-c5ls7veng3ljfc5g.us.auth0.com"
+        clientId="NeusqrLxWz03fI3MizB5jB0CsNHROijk"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Auth0Provider>,
+);
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../components/home/header.jsx";
 import Body from "../components/home/body.jsx";
+import LoginButton from "../components/home/login.jsx";
 
 function Home() {
     return (
         <div className="home">
+            <LoginButton></LoginButton>
             <Header />
             <Body/>
         </div>
