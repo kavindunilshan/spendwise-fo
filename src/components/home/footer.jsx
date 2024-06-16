ef8c79a9ca3a2c5d1129d93de931c689e16fb3f0
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-16 17:24:23 +0530

    Footer created

--- src/components/home/footer.jsx
+++ src/components/home/footer.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import "/src/styles/home/footer.css";
+import {Facebook, Instagram, LinkedIn, Twitter} from "@mui/icons-material";
 
 function Footer() {
     return (
-        <div className={'home-footer'}>
+        <div className={'home-footer-container'}>
+            <div className={'home-footer-content-left'}>
+                <img src={'./src/assets/logoa.png'} alt={'home-base-image'} className={'home-footer-logo'}/>
+                <div className={'home-footer-slogan'}>Personal Expense Tracker</div>
+            </div>
+            <div className={'home-footer-content-right'}>
+                <div className={'home-footer-socials'}>
+                    <a href={'https://www.facebook.com'} className={'home-footer-social-icon'}>
+                        <LinkedIn style={{color: "#320440", fontSize: '40px'}}/>
+                    </a>
+                    <a href={'https://www.facebook.com'} className={'home-footer-social-icon'}>
+                        <Facebook style={{color: "#320440", fontSize: '40px'}}/>
+                    </a>
+                    <a href={'https://www.instagram.com'} className={'home-footer-social-icon'}>
+                        <Instagram style={{color: "#320440", fontSize: '40px'}}/>
+                    </a>
+                    <a href={'https://www.twitter.com'} className={'home-footer-social-icon'}>
+                        <Twitter style={{color: "#320440", fontSize: '40px'}}/>
+                    </a>
+                </div>
+                <div className={'home-footer-legal'}>
+                    <div className={'home-footer-legal-item'}>Privacy Policy</div>
+                    <div className={'home-footer-legal-item'}>Terms of Service</div>
+                    <div className={'home-footer-legal-item'}>Contact Us</div>
+                </div>
+                <div className={'home-footer-rights-text'}>
+                    © 2024 SpendWise. Empowering you to take control of your finances. All Rights Received.
+                </div>
+            </div>
 
         </div>
     );
