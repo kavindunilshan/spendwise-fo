b39b9db32f9855d6229c037ce1e72cc39d363c91
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-17 22:56:27 +0530

    Settings section

---
+++ src/pages/settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SettingsLayout from "../components/settings/side-menu.jsx";
+
+function Settings(props) {
+    return (
+        <div>
+            <SettingsLayout/>
+        </div>
+    );
+}
+
+export default Settings;
