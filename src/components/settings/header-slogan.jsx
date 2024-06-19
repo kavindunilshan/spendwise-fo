e8eb1aed55465a24379ffd99c58f33ca7a7bcf10
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-19 17:42:24 +0530

    Header changed.

--- src/components/settings/header-slogan.jsx
+++ src/components/settings/header-slogan.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import '/src/styles/settings/header-slogan.css';
 
-function HeaderWithSlogan({title, slogan, width, titleStyle}) {
+function HeaderWithSlogan({title, slogan, isSubTopic, titleStyle}) {
     return (
         <div className={'settings-header'}>
             <div className={'settings-header-title'} style={titleStyle || {}}>{title}</div>
-            <div className={'settings-header-slogan'} style={{fontSize: width ? '0.8em': '1em'}}>{slogan}</div>
-            <hr className={'settings-header-separator'} width={width || '100%'}/>
+            <div className={'settings-header-slogan'} style={{fontSize: isSubTopic ? '0.8em': '1em'}}>{slogan}</div>
         </div>
     );
 }
