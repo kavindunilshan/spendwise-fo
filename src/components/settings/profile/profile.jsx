9040303e22ed25e1d4a934f6d4f0583d10225f1c
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-21 20:20:28 +0530

    Profile page done.

--- src/components/settings/profile/profile.jsx
+++ src/components/settings/profile/profile.jsx
@@ -3,13 +3,23 @@ import {SettingsContext} from "../settings-context.jsx";
 import '/src/styles/settings/components/profile.css';
 import {CountryDropdown} from "react-country-region-selector";
 import Button from "@mui/material/Button";
+import {useAuth0} from "@auth0/auth0-react";
+import {Avatar} from "@mui/material";
 
 function Profile({}) {
 
     const { setComponentData } = useContext(SettingsContext);
 
+    let { user, logout } = useAuth0();
+
     const [country, setCountry] = React.useState('');
     const [isEditing, setIsEditing] = React.useState(false);
+    const [currency, setCurrency] = React.useState([]);
+
+    const btnStyle = {
+        margin: '10px',
+        backgroundColor: '#320440',
+    }
 
     useEffect(() => {
         setComponentData({"title": "Profile", "slogan": "View and edit your profile information"});
@@ -23,44 +33,59 @@ function Profile({}) {
         setIsEditing(false);
     }
 
+    const handleCurrencyChange = (event) => {
+        const value = event.target.value;
+        setCurrency(value);
+    }
+
     return (
         <div className={'profile-container'}>
             <div className={'profile-content'}>
                 <div className={'profile-pic'}>
-                    <img src={'/src/assets/images/profile.png'} alt={'profile'}/>
+                    <Avatar className={'login-user-icon'} style={{width: 140, height: 140, cursor: 'pointer'}}
+                            alt={user?.name || ""} src={user?.picture || ""}/>
                 </div>
                 <div className={'profile-content-item'}>
                     <label htmlFor={'profile-name'}>Name</label>
-                    <input type={'text'} id={'profile-name'} name={'profile-name'} placeholder={'Your name'}/>
+                    <input disabled={!isEditing} type={'text'} id={'profile-name'} name={'profile-name'}
+                           placeholder={'Your name'}/>
                 </div>
                 <div className={'profile-content-item'}>
                     <label htmlFor={'profile-email'}>Email</label>
-                    <input type={'email'} id={'profile-email'} name={'profile-email'} placeholder={'Your email'}/>
+                    <input disabled={!isEditing} type={'email'} id={'profile-email'} name={'profile-email'}
+                           placeholder={'Your email'}/>
                 </div>
                 <div className={'profile-content-item'}>
                     <label htmlFor={'profile-city'}>Country</label>
                     <CountryDropdown
                         value={country}
                         onChange={(val) => selectCountry(val)}
-                        // blue when focus
-                        style={{width: '30%', height: '40px', padding: '5px', borderRadius: '5px', border: '1px solid #ccc', outline: 'none'}}
+                        disabled={!isEditing}
+                        style={{
+                            width: '30%',
+                            height: '40px',
+                            padding: '5px',
+                            borderRadius: '5px',
+                            border: '1px solid #ccc',
+                            outline: 'none'
+                        }}
                     />
                 </div>
                 <div className={'profile-content-item'}>
-                    <label htmlFor={'profile-zip'}>Currency</label>
-                    <input type={'text'} id={'profile-zip'} name={'profile-currency'} placeholder={'Your currency'}/>
+                    <label htmlFor={'profile-currency'}>Currency</label>
+                    <input disabled={!isEditing} type={'text'} id={'profile-currency'} name={'profile-currency'}
+                           placeholder={'Currency (max 3 letters)'}
+                           onChange={handleCurrencyChange} value={currency} maxLength={3}
+                    />
                 </div>
-                <div className={'profile-content-item'}>
+                <div className={'profile-btns'}>
+                    {!isEditing ? <Button style={btnStyle} variant="contained" onClick={() => setIsEditing(true)}>Edit</Button>
+                        : <>
+                            <Button style={btnStyle} variant="contained" onClick={() => handleSave()}>Save</Button>
+                            <Button style={btnStyle} variant="contained" onClick={() => setIsEditing(false)}>Cancel</Button>
+                        </>
+                    }
                 </div>
-            </div>
-
-            <div className={'profile-btns'}>
-                {!isEditing ? <Button variant="contained" onClick={() => setIsEditing(true)}>Edit</Button>
-                    : <>
-                        <Button variant="contained" onClick={() => handleSave()}>Save</Button>
-                        <Button variant="contained" onClick={() => setIsEditing(false)}>Cancel</Button>
-                     </>
-                }
             </div>
         </div>
     );
