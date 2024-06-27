7e9d2950786c04239fb91d6ed064ce579c69ed2f
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-06-27 20:04:40 +0530

    Icons styled.

--- src/components/data-center/flat-icons.jsx
+++ src/components/data-center/flat-icons.jsx
@@ -15,29 +15,29 @@ function FlatIcons({}) {
             <List sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', padding: 0 }}>
                 <ListItem button sx={{ display: 'flex', justifyContent: 'center', padding: '1rem', '&:hover': { backgroundColor: 'transparent' } }}>
                     <Tooltip title="Home" placement="top">
-                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)' }}>
-                            <HomeIcon style={{fontSize: 25}} onClick={() => handleClick("/")}/>
+                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)', marginLeft: '5px', '&:hover': { color: '#68066d' }  }}>
+                            <HomeIcon style={{fontSize: 30}} onClick={() => handleClick("/")}/>
                         </ListItemIcon>
                     </Tooltip>
                 </ListItem>
                 <ListItem button sx={{ display: 'flex', justifyContent: 'center', padding: '1rem', '&:hover': { backgroundColor: 'transparent' } }}>
                     <Tooltip title="Home" placement="top">
-                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)' }}>
-                            <Settings style={{fontSize: 25}} onClick={() => handleClick("/settings/profile")}/>
+                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)', marginLeft: '5px', '&:hover': { color: '#68066d' }  }}>
+                            <Settings style={{fontSize: 30}} onClick={() => handleClick("/settings/profile")}/>
                         </ListItemIcon>
                     </Tooltip>
                 </ListItem>
                 <ListItem button sx={{ display: 'flex', justifyContent: 'center', padding: '1rem', '&:hover': { backgroundColor: 'transparent' } }}>
                     <Tooltip title="Data Center" placement="top">
-                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)' }}>
-                            <TextSnippet style={{fontSize: 25}} onClick={() => handleClick("/data-center")}/>
+                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)', marginLeft: '5px', '&:hover': { color: '#68066d' }  }}>
+                            <TextSnippet style={{fontSize: 30}} onClick={() => handleClick("/data-center")}/>
                         </ListItemIcon>
                     </Tooltip>
                 </ListItem>
                 <ListItem button sx={{ display: 'flex', justifyContent: 'center', padding: '1rem', '&:hover': { backgroundColor: 'transparent' } }}>
                     <Tooltip title="Receipt" placement="top">
-                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)' }}>
-                            <UserMenu size={25}/>
+                        <ListItemIcon sx={{ minWidth: 'auto', color: 'var(--text-color)', marginLeft: '5px', '&:hover': { color: '#68066d' } }}>
+                            <UserMenu size={30}/>
                         </ListItemIcon>
                     </Tooltip>
                 </ListItem>
--- src/pages/data-center.jsx
+++ src/pages/data-center.jsx
@@ -5,7 +5,9 @@ import HeaderWithSlogan from "../components/settings/header-slogan.jsx";
 import {SettingsContext} from "../components/settings/settings-context.jsx";
 import {NavigateNext} from "@mui/icons-material";
 import DataMenu from "../components/data-center/data-menu.jsx";
-import FlatIcons from "../components/dashboard/flat-icons.jsx";
+import '/src/styles/data-center/colors.css'
+import '/src/styles/data-center/data-center.css'
+import FlatIcons from "../components/data-center/flat-icons.jsx";
 
 function DataCenter() {
 
@@ -13,7 +15,7 @@ function DataCenter() {
 
 
     return (
-        <div className={'settings'}>
+        <div className={'settings colors'}>
             <div className={'settings-topics'}>
                 <HeaderWithSlogan title={'Data Center'} slogan={'SpendWise data center'}/>
                 <NavigateNext style={{color: '#6c757d', margin: '0 20px 0 50px'}}/>
@@ -24,7 +26,7 @@ function DataCenter() {
                     titleStyle={{fontWeight: '600', fontStyle: 'italic', fontSize: '1.7em',}}
                 />
 
-                <div className={'settings-flat-icons'}>
+                <div className={'data-center-flat-icons'}>
                     <FlatIcons/>
                 </div>
             </div>
