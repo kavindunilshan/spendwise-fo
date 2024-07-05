d60721075eaf772ae91731eb8bfbc488c0266a52
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-07-06 00:02:25 +0530

    Tokens handled and send to local storage.

--- src/services/axios-services.js
+++ src/services/axios-services.js
@@ -1,23 +1,48 @@
 import axios from "axios";
+import getAccessToken from "./backend-token.js";
 
 const baseUrl = "http://localhost:8080/api/private";
 
+// fetch all categories
 export const fetchAllCategories = async () => {
-    const response = await axios.get(baseUrl + "/categories");
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(baseUrl + "/categories", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
+// fetch category by type
 export const fetchCategoryByType = async (type) => {
-    const response = await axios.get(baseUrl + "/categories/type/" + type);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(baseUrl + "/categories/type/" + type, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
+// create a transaction
 export const createTransaction = async (transaction) => {
-    const response = await axios.post(baseUrl + "/transactions", transaction);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.post(baseUrl + "/transactions", transaction, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response.data;
 }
 
-// edit
+// edit a transaction
 export const editTransaction = async (tid, transaction) => {
-    const response = await axios.put(baseUrl + "/transactions/" + tid, transaction);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.put(baseUrl + "/transactions/" + tid, transaction, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
--- src/services/dashboard.js
+++ src/services/dashboard.js
@@ -1,39 +1,70 @@
 import axios from "axios";
+import getAccessToken from "./backend-token.js";
 
 const baseUrl = "http://localhost:8080/api/private";
 
 // fetch last five transactions
 export const fetchLastFiveTransactions = async (userId) => {
-    const response = await axios.get(`${baseUrl}/transactions/${userId}/last-five`);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(`${baseUrl}/transactions/${userId}/last-five`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
 // fetch all transactions
 export const fetchAllTransactions = async (userId) => {
-    const response = await axios.get(`${baseUrl}/transactions/${userId}`);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(`${baseUrl}/transactions/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
 // data breakdown
 export const fetchExpenseBreakdown = async (userId, type) => {
-    const response = await axios.get(`${baseUrl}/transactions/${userId}/${type}/breakdown`);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(`${baseUrl}/transactions/${userId}/${type}/breakdown`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
 // post a user
 export const createUser = async (user) => {
-    const response = await axios.post(baseUrl + "/users", user);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.post(`${baseUrl}/users`, user, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
 // fetch pocket balance
 export const fetchPocketBalance = async (userId) => {
-    const response = await axios.get(`${baseUrl}/transactions/pocket/${userId}`);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(`${baseUrl}/transactions/pocket/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
-// @GetMapping("/{userId}/monthly/{type}/{months}")
+// fetch over monthly data
 export const fetchOverMonthlyData = async (userId, months) => {
-    const response = await axios.get(`${baseUrl}/transactions/${userId}/monthly/${months}`);
+    const token = await getAccessToken(); // Wait for token to be fetched
+    const response = await axios.get(`${baseUrl}/transactions/${userId}/monthly/${months}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
