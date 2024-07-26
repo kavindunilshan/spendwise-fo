d36438c0b89e456beb5013e33c17a04432778c78
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-07-26 14:20:25 +0530

    Result section created.

--- src/components/data-center/components/advices/advices.jsx
+++ src/components/data-center/components/advices/advices.jsx
@@ -3,6 +3,7 @@ import HeaderWithSlogan from "../../../settings/header-slogan.jsx";
 import '/src/styles/data-center/advice.css';
 import {SettingsContext} from "../../../settings/settings-context.jsx";
 import RequestForm from "./request-form.jsx";
+import AdviceResult from "./result.jsx";
 
 function Advices(props) {
 
@@ -20,8 +21,26 @@ function Advices(props) {
         fontWeight: '500',
         fontStyle: 'italic',
         color: '#04504d'
-
     }
+
+    const titleStyle2 = {
+        fontSize: '1.7em',
+        fontWeight: '500',
+        fontStyle: 'italic',
+        color: '#04504d'
+    }
+
+    const advice = {
+        title: 'Thread 1',
+        status: 'Processing',
+        timestamp: '2024-07-26 14:30',
+        problem: 'This is the problem description.',
+        result: 'This is the result of the advice.',
+        answerImage: './src/assets/b1.jpg',
+        resultImage: './src/assets/b2.jpg'
+    };
+
+
 
     return (
         <div className={'advices'}>
@@ -32,6 +51,15 @@ function Advices(props) {
                     />
 
                     <RequestForm/>
+                </div>
+                <div className={'advices-info'}>
+                    <HeaderWithSlogan title={'WiseAdvices'}
+                                      isSubTopic={true}
+                                      slogan={'The advice that makes you a financial genius.'}
+                                      titleStyle={titleStyle}
+                    />
+
+                    <AdviceResult {...advice}/>
                 </div>
             </div>
         </div>
