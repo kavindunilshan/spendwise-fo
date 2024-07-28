58a5ec76538bb7ebbd504646c23298e6751239f8
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-07-28 12:18:49 +0530

    Made footer responsive.

--- src/components/home/footer.jsx
+++ src/components/home/footer.jsx
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "/src/styles/home/footer.css";
 import {Facebook, Instagram, LinkedIn, Twitter} from "@mui/icons-material";
 
 function Footer() {
-    return (
-        <div className={'home-footer-container'}>
-            <div className={'home-footer-content-left'}>
-                <img src={'./src/assets/logoa.png'} alt={'home-base-image'} className={'home-footer-logo'}/>
-                <div className={'home-footer-slogan'}>Personal Expense Tracker</div>
-            </div>
-            <div className={'home-footer-content-right'}>
-                <div className={'home-footer-socials'}>
-                    <a href={'https://www.linkedin.com'} target="_blank" className={'home-footer-social-icon'}>
-                        <LinkedIn style={{color: "#fff", fontSize: '40px'}}/>
-                    </a>
-                    <a href={'https://www.facebook.com'} target="_blank" className={'home-footer-social-icon'}>
-                        <Facebook style={{color: "#fff", fontSize: '40px'}}/>
-                    </a>
-                    <a href={'https://www.instagram.com'} target="_blank" className={'home-footer-social-icon'}>
-                        <Instagram style={{color: "#fff", fontSize: '40px'}}/>
-                    </a>
-                    <a href={'https://www.twitter.com'} target="_blank" className={'home-footer-social-icon'}>
-                        <Twitter style={{color: "#fff", fontSize: '40px'}}/>
-                    </a>
-                </div>
-                <div className={'home-footer-legal'}>
-                    <div className={'home-footer-legal-item'}>Privacy Policy</div>
-                    <div className={'home-footer-legal-item'}>Terms of Service</div>
-                    <div className={'home-footer-legal-item'}>Contact Us</div>
-                </div>
-                <div className={'home-footer-rights-text'}>
-                    © 2024 SpendWise. Empowering you to take control of your finances. All Rights Received.
-                </div>
-            </div>
 
+    const [isWidthThresholdPassed, setIsWidthThresholdPassed] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsWidthThresholdPassed(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    return (
+        <div className={'home-footer'}>
+            <div className={`home-footer-container ${isWidthThresholdPassed ? 'hf-c-small': ''}`}>
+                <div className={'home-footer-content-left'}>
+                    <img src={'./src/assets/logoa.png'} alt={'home-base-image'} className={'home-footer-logo'}/>
+                    <div className={'home-footer-slogan'}>Personal Expense Tracker</div>
+                </div>
+                <div className={'home-footer-content-right'}>
+                    <div className={'home-footer-socials'}>
+                        <a href={'https://www.linkedin.com'} target="_blank" className={'home-footer-social-icon'}>
+                            <LinkedIn style={{color: "#fff", fontSize: '40px'}}/>
+                        </a>
+                        <a href={'https://www.facebook.com'} target="_blank" className={'home-footer-social-icon'}>
+                            <Facebook style={{color: "#fff", fontSize: '40px'}}/>
+                        </a>
+                        <a href={'https://www.instagram.com'} target="_blank" className={'home-footer-social-icon'}>
+                            <Instagram style={{color: "#fff", fontSize: '40px'}}/>
+                        </a>
+                        <a href={'https://www.twitter.com'} target="_blank" className={'home-footer-social-icon'}>
+                            <Twitter style={{color: "#fff", fontSize: '40px'}}/>
+                        </a>
+                    </div>
+                    <div className={'home-footer-legal'}>
+                        <div className={'home-footer-legal-item'}>Privacy Policy</div>
+                        <div className={'home-footer-legal-item'}>Terms of Service</div>
+                        <div className={'home-footer-legal-item'}>Contact Us</div>
+                    </div>
+                </div>
+            </div>
+            <div className={'home-footer-rights-text'}>
+                © 2024 SpendWise. Empowering you to take control of your finances. All Rights Received.
+            </div>
         </div>
-    );
+)
+    ;
 }
 
 export default Footer;
