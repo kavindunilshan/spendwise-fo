87e0142f50d3050c85e43e5b189a6ecae7d6b4f3
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-08-26 22:57:20 +0530

    Financial education

--- src/components/data-center/components/learn/learn.jsx
+++ src/components/data-center/components/learn/learn.jsx
@@ -1,7 +1,8 @@
 import React, {useContext, useEffect} from 'react';
 import {SettingsContext} from "../../../settings/settings-context.jsx";
+import HeaderWithSlogan from "../../../settings/header-slogan.jsx";
 
-function Learn(props) {
+function Learn({}) {
 
     const { setComponentData } = useContext(SettingsContext);
 
@@ -9,8 +10,34 @@ function Learn(props) {
         setComponentData({"title": "WiseLearn", "slogan": "SpendWise financial education platform"});
     }, []);
 
+    const titleStyle = {
+        fontSize: '1.7em',
+        fontWeight: '500',
+        fontStyle: 'italic',
+        color: '#320440'
+    }
+
     return (
-        <div></div>
+        <div className={'learn'}>
+            <HeaderWithSlogan
+                title={'Welcome to WiseLearn'}
+                titleStyle={titleStyle}
+            />
+            <div className={'learn-content'}>
+                <div className={'learn-item'}>
+                    <div className={'learn-intro-text'}>
+                        At SpendWise, we believe that knowledge is key to financial well-being.
+                        WiseLearn is our dedicated feature designed to empower you with essential
+                        financial education. Explore a wealth of resources, tips, and strategies
+                        that will help you manage your finances more effectively, make
+                        informed decisions, and achieve your financial goals.
+                        Dive into our curated content and take control of
+                        your financial future today!
+                    </div>
+                </div>
+                <img className={'learn-image'} src={'/src/assets/finance.png'} alt={'learn'}/>
+            </div>
+        </div>
     );
 }
 
