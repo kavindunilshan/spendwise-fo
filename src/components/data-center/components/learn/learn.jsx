91889e191b866f7aa5b6b399aec0db5546b19391
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-08-26 23:48:25 +0530

    Financial education created.

--- src/components/data-center/components/learn/learn.jsx
+++ src/components/data-center/components/learn/learn.jsx
@@ -1,6 +1,8 @@
 import React, {useContext, useEffect} from 'react';
 import {SettingsContext} from "../../../settings/settings-context.jsx";
 import HeaderWithSlogan from "../../../settings/header-slogan.jsx";
+import '/src/styles/data-center/learn.css';
+import TextImageContainer from "../../../home/text-image.jsx";
 
 function Learn({}) {
 
@@ -9,6 +11,15 @@ function Learn({}) {
     useEffect(() => {
         setComponentData({"title": "WiseLearn", "slogan": "SpendWise financial education platform"});
     }, []);
+
+    const description = 'At SpendWise, we believe that knowledge is key to financial well-being.\n' +
+        '                        WiseLearn is our dedicated feature designed to empower you with essential\n' +
+        '                        financial education. Explore a wealth of resources, tips, and strategies\n' +
+        '                        that will help you manage your finances more effectively, make\n' +
+        '                        informed decisions, and achieve your financial goals.\n' +
+        '                        Dive into our curated content and take control of\n' +
+        '                        your financial future today!'
+
 
     const titleStyle = {
         fontSize: '1.7em',
@@ -19,21 +30,10 @@ function Learn({}) {
 
     return (
         <div className={'learn'}>
-            <HeaderWithSlogan
-                title={'Welcome to WiseLearn'}
-                titleStyle={titleStyle}
-            />
             <div className={'learn-content'}>
                 <div className={'learn-item'}>
-                    <div className={'learn-intro-text'}>
-                        At SpendWise, we believe that knowledge is key to financial well-being.
-                        WiseLearn is our dedicated feature designed to empower you with essential
-                        financial education. Explore a wealth of resources, tips, and strategies
-                        that will help you manage your finances more effectively, make
-                        informed decisions, and achieve your financial goals.
-                        Dive into our curated content and take control of
-                        your financial future today!
-                    </div>
+                    <TextImageContainer isLeft={true} title={'Welcome to WiseLearn'}
+                                        description={description}/>
                 </div>
                 <img className={'learn-image'} src={'/src/assets/finance.png'} alt={'learn'}/>
             </div>
