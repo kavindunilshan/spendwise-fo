8baad85a8ce569832a08d3f1e92d4a08412fe23a
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-11-01 23:10:14 +0530

    ErrorBoundary done.

--- src/components/utils/ErrorBoundary.jsx
+++ src/components/utils/ErrorBoundary.jsx
@@ -1,33 +1,33 @@
-// src/components/ErrorBoundary.js
 import React, { Component } from 'react';
+import "/src/styles/utils/error-boundary.css";
 
 class ErrorBoundary extends Component {
     state = { hasError: false };
 
     static getDerivedStateFromError(error) {
-        
         return { hasError: true };
     }
 
     componentDidCatch(error, errorInfo) {
-        
     }
 
     handleReload = () => {
         this.setState({ hasError: false });
-        window.location.reload();
+        window.location.href = window.location.origin;
     };
+
 
     render() {
         if (this.state.hasError) {
             return (
-                <div style={{ textAlign: 'center', padding: '50px' }}>
+                <div className="eb-error-container">
+                    <img src="/logoa.png" alt="SpendWise Logo"/>
                     <h1>Something went wrong!</h1>
                     <p>
                         We apologize for the inconvenience.
                         Please try refreshing the page or come back later.
                     </p>
-                    <button onClick={this.handleReload} style={{ padding: '10px 20px', cursor: 'pointer' }}>
+                    <button onClick={this.handleReload} className="eb-error-button">
                         Reload Page
                     </button>
                 </div>
